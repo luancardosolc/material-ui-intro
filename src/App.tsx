8b2914b8556caa7bcc3095bcf3ff629dd6d4c8b2
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-30 22:07:59 -0300

    ButtonGroup example

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, ButtonGroup } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          variant="contained"
-          color="secondary"
-          href="#"
-          size="large"
-          style={{
-            fontSize: 15,
-          }}
-          startIcon={<SaveIcon />}
-        >
-          Icon on the left
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          href="#"
-          size="large"
-          style={{
-            marginTop: 20,
-            fontSize: 15,
-          }}
-          endIcon={<SaveIcon />}
-        >
-          Icon on the right
-        </Button>
+        <ButtonGroup>
+          <Button
+            variant="contained"
+            color="primary"
+            href="#"
+            size="large"
+            style={{
+              fontSize: 15,
+            }}
+            startIcon={<SaveIcon />}
+          >
+            Icon on the left
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            href="#"
+            size="large"
+            style={{
+              fontSize: 15,
+            }}
+            endIcon={<SaveIcon />}
+          >
+            Icon on the right
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
