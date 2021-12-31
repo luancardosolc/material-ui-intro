75743db3ed8e8a9acdb2155f2dfcadebf3ae7c98
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-30 22:22:39 -0300

    Checkbox component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup } from '@mui/material';
+import { Button, ButtonGroup, Checkbox, FormControlLabel } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 
 function App() {
+  const [checkbox, setCheckbox] = useState(true);
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckbox(event.target.checked);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <FormControlLabel
+          label="Check-me darling"
+          control={
+            <Checkbox
+              checked={checkbox} 
+              onChange={handleCheckboxChange}
+              color='error'
+            />
+          }
+        />
         <ButtonGroup>
           <Button
             variant="contained"
