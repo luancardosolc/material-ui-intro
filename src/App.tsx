eab7b5c3c9d8d259a7ae8ab3c637ab3b4bf1ca5d
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-30 21:59:47 -0300

    Example of Button Icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           style={{
             fontSize: 15,
           }}
+          startIcon={<SaveIcon />}
         >
           I am a Button!
         </Button>
