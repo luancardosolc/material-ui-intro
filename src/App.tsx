52dd7b095b3719a77dbd120a81b0d09836ebf2ea
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-02 16:57:38 -0300

    Container example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField, Typography } from '@mui/material';
+import { Button, ButtonGroup, Checkbox, Container, FormControlLabel, TextField, Typography } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { makeStyles } from '@mui/styles';
@@ -45,64 +45,66 @@ function App() {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={theme}>  
-      <div className="App">
-        <header className="App-header">
-          <Typography
-            variant="h2"
-            component="div"
-          >
-            Welcome
-          </Typography>
-          <Button className={classes.root} >buttonStyled</Button>
-          <TextField
-            variant="outlined"
-            color="secondary"
-            type="email"
-            label="Email input"
-            placeholder="test@test.com"
-          />
-          <FormControlLabel
-            label="Check-me darling"
-            control={
-              <Checkbox
-                checked={checkbox} 
-                onChange={handleCheckboxChange}
-                color='error'
-                checkedIcon={<SaveIcon />}
-                icon={<DeleteIcon />}
-              />
-            }
-          />
-          <ButtonGroup>
-            <Button
-              variant="contained"
-              color="primary"
-              href="#"
-              size="large"
-              style={{
-                fontSize: 15,
-              }}
-              startIcon={<SaveIcon />}
+    <ThemeProvider theme={theme}>
+      <Container>
+        <div className="App">
+          <header className="App-header">
+            <Typography
+              variant="h2"
+              component="div"
             >
-              Icon on the left
-            </Button>
-            <Button
-              variant="contained"
+              Welcome
+            </Typography>
+            <Button className={classes.root} >buttonStyled</Button>
+            <TextField
+              variant="outlined"
               color="secondary"
-              href="#"
-              size="large"
-              style={{
-                fontSize: 15,
-              }}
-              endIcon={<SaveIcon />}
-            >
-              Icon on the right
-            </Button>
-          </ButtonGroup>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+              type="email"
+              label="Email input"
+              placeholder="test@test.com"
+            />
+            <FormControlLabel
+              label="Check-me darling"
+              control={
+                <Checkbox
+                  checked={checkbox} 
+                  onChange={handleCheckboxChange}
+                  color='error'
+                  checkedIcon={<SaveIcon />}
+                  icon={<DeleteIcon />}
+                />
+              }
+            />
+            <ButtonGroup>
+              <Button
+                variant="contained"
+                color="primary"
+                href="#"
+                size="large"
+                style={{
+                  fontSize: 15,
+                }}
+                startIcon={<SaveIcon />}
+              >
+                Icon on the left
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                href="#"
+                size="large"
+                style={{
+                  fontSize: 15,
+                }}
+                endIcon={<SaveIcon />}
+              >
+                Icon on the right
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
     </ThemeProvider>
   );
 }
