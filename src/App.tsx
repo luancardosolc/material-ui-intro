92011c0c6dc4c7581475989e792040fbe8c8d92a
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-02 10:34:19 -0300

    Installing mui/styles + makeStyles example

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,36 @@ import './App.css';
 import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { makeStyles } from '@mui/styles';
 
 function App() {
   const [checkbox, setCheckbox] = useState(true);
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCheckbox(event.target.checked);
   };
+  
+  const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #333, #999)',
+      border: 0,
+      borderRadius: 15,
+      color: 'white',
+      padding: '0 30px',
+    },
+  });
+  const classes = useStyles();
 
   return (
     <div className="App">
       <header className="App-header">
+        <Button className={classes.root} >buttonStyled</Button>
         <TextField
           variant="outlined"
           color="secondary"
           type="email"
           label="Email input"
           placeholder="test@test.com"
-        ></TextField>
+        />
         <FormControlLabel
           label="Check-me darling"
           control={
